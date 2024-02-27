c4acc64daad36f18fae9c445609fd9ef198117fc
lucasmolinari <lucas.molinari2003@gmail.com>
2024-02-27 16:07:27 -0300

    Minor fixes before Docker and NGINX setup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {
   updateLastTransactions,
 } from "./db/db.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 9999;
 const app = express();
 
 app.use(express.json());
@@ -15,39 +15,42 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/", (_, res) => {
-  res.send("API works").status(200);
-});
-
+// POST /clientes/[id]/transacoes
 app.post("/clientes/:id/transacoes", async (req, res) => {
   const id = req.params.id;
   const body = req.body;
   const isValid = await isBodyValid(body, id);
   if (!isValid) {
-    return res.status(400).json({ message: "Dados inválidos" });
+    return res.status(422).json({ message: "Dados inválidos" });
   }
-  
+
   const user = await getUser(id);
   if (user === null) {
     return res.status(404).json({ message: "Cliente não encontrado" });
   }
-  const novoSaldo = user.saldo - body.valor;
-  if (body.tipo === "d" && novoSaldo < -user.limite) {
-    return res.status(422).json({ message: "Limite insuficiente" });
+  let newBalance;
+  if (body.tipo === "d") {
+    if (user.saldo - body.valor < -user.limite) {
+      return res.status(422).json({ message: "Limite insuficiente" });
+    }
+    newBalance = user.saldo - body.valor;
+  } else {
+    newBalance = user.saldo + body.valor;
   }
-  await updateBalance(id, novoSaldo);
+
+  await updateBalance(id, newBalance);
   await updateLastTransactions(id, {
     valor: body.valor,
     tipo: body.tipo,
     descricao: body.descricao,
     realizada_em: new Date().toISOString(),
   });
-  return res.status(200).json({ limite: user.limite, saldo: novoSaldo });
+  return res.status(200).json({ limite: user.limite, saldo: newBalance });
 });
 
 // Extrato: GET /clientes/[id]/extrato
 app.get("/clientes/:id/extrato", async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   const user = await getUser(id);
   if (user === null) {
     return res.status(404).json({ message: "Cliente não encontrado" });
@@ -74,7 +77,10 @@ async function isBodyValid(body, id) {
   if (body.tipo !== "d" && body.tipo !== "c") {
     return false;
   }
-  if (isNaN(body.valor) || isNaN(parseFloat(body.valor))) {
+  if (typeof body.valor !== "number") {
+    return false;
+  }
+  if (body.descricao === "" || body.descricao.length > 10) {
     return false;
   }
   return true;
