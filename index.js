e8247da2ba27e28a8605032669d4ec3f63f907a5
lucasmolinari <lucas.molinari2003@gmail.com>
2024-02-28 13:36:27 -0300

    Add cluster module

--- index.js
+++ index.js
@@ -1,87 +1,25 @@
 import express from "express";
-import {
-  getUser,
-  updateBalance,
-  resetUsers,
-  updateLastTransactions,
-} from "./db/db.js";
-
+import router from "./src/api.js";
+import cluster from "cluster";
+import os from "os";
+const cpus = os.cpus().length;
 const port = process.env.PORT || 9999;
 const app = express();
-
 app.use(express.json());
 app.set("port", port);
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use(router)
 
-// POST /clientes/[id]/transacoes
-app.post("/clientes/:id/transacoes", async (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  const isValid = await isBodyValid(body, id);
-  if (!isValid) {
-    return res.status(422).json({ message: "Dados inválidos" });
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
   }
-
-  const user = await getUser(id);
-  if (user === null) {
-    return res.status(404).json({ message: "Cliente não encontrado" });
-  }
-  let newBalance;
-  if (body.tipo === "d") {
-    if (user.saldo - body.valor < -user.limite) {
-      return res.status(422).json({ message: "Limite insuficiente" });
-    }
-    newBalance = user.saldo - body.valor;
-  } else {
-    newBalance = user.saldo + body.valor;
-  }
-
-  await updateBalance(id, newBalance);
-  await updateLastTransactions(id, {
-    valor: body.valor,
-    tipo: body.tipo,
-    descricao: body.descricao,
-    realizada_em: new Date().toISOString(),
+  cluster.on("exit", (worker, _c, _s) => {
+    console.log(`Worker ${worker.process.pid} died`);
   });
-  return res.status(200).json({ limite: user.limite, saldo: newBalance });
-});
-
-// Extrato: GET /clientes/[id]/extrato
-app.get("/clientes/:id/extrato", async (req, res) => {
-  const id = req.params.id;
-  const user = await getUser(id);
-  if (user === null) {
-    return res.status(404).json({ message: "Cliente não encontrado" });
-  }
-  const balance = {
-    total: user.saldo,
-    data_extrato: new Date().toISOString(),
-    limite: user.limite,
-  };
-  res
-    .status(200)
-    .json({ saldo: balance, ultimas_transacoes: user.ultimas_transacoes });
-});
-
-app.delete("/reset", async (req, res) => {
-  await resetUsers();
-  res.status(200).json({ message: "Users reseted" });
-});
-
-async function isBodyValid(body, id) {
-  if (isNaN(id) || isNaN(parseFloat(id))) {
-    return false;
-  }
-  if (body.tipo !== "d" && body.tipo !== "c") {
-    return false;
-  }
-  if (typeof body.valor !== "number") {
-    return false;
-  }
-  if (body.descricao === "" || body.descricao.length > 10) {
-    return false;
-  }
-  return true;
+} else {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    console.log(`Worker ${process.pid} started`)
+  });
 }
---
+++ src/api.js
@@ -0,0 +1,83 @@
+import { Router } from "express";
+
+import {
+  getUser,
+  updateBalance,
+  resetUsers,
+  updateLastTransactions,
+} from "./db.js";
+
+export const router = Router();
+// POST /clientes/[id]/transacoes
+router.post("/clientes/:id/transacoes", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const isValid = await isBodyValid(body, id);
+  if (isValid !== true) {
+    return res.status(422).json({ message: `Dados inválidos: ${isValid}` });
+  }
+
+  const user = await getUser(id);
+  if (user === null) {
+    return res.status(404).json({ message: "Cliente não encontrado" });
+  }
+  let newBalance;
+  if (body.tipo === "d") {
+    if (user.saldo - body.valor < -user.limite) {
+      return res.status(422).json({ message: "Limite insuficiente" });
+    }
+    newBalance = user.saldo - body.valor;
+  } else {
+    newBalance = user.saldo + body.valor;
+  }
+
+  await updateBalance(id, newBalance);
+  await updateLastTransactions(id, {
+    valor: body.valor,
+    tipo: body.tipo,
+    descricao: body.descricao,
+    realizada_em: new Date().toISOString(),
+  });
+  return res.status(200).json({ limite: user.limite, saldo: newBalance });
+});
+
+// Extrato: GET /clientes/[id]/extrato
+router.get("/clientes/:id/extrato", async (req, res) => {
+  const id = req.params.id;
+  const user = await getUser(id);
+  if (user === null) {
+    return res.status(404).json({ message: "Cliente não encontrado" });
+  }
+  const balance = {
+    total: user.saldo,
+    data_extrato: new Date().toISOString(),
+    limite: user.limite,
+  };
+  res
+    .status(200)
+    .json({ saldo: balance, ultimas_transacoes: user.ultimas_transacoes });
+});
+
+router.delete("/reset", async (req, res) => {
+  await resetUsers();
+  res.status(200).json({ message: "Users reseted" });
+});
+
+async function isBodyValid(body, id) {
+  if (isNaN(id) || isNaN(parseFloat(id))) {
+    return "id";
+  }
+  if (body.tipo !== "d" && body.tipo !== "c") {
+    console.log(body.tipo)
+    return "tipo";
+  }
+  if (typeof body.valor !== "number") {
+    return "valor";
+  }
+  if (body.descricao === "" || body.descricao.length > 10) {
+    return "desc";
+  }
+  return true;
+}
+
+export default router;
