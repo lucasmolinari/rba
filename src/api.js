1e78b311aaf8bbf198ed07076b7ea4b4df8df1d9
lucasmolinari <lucas.molinari2003@gmail.com>
2024-03-03 11:30:34 -0300

    Remove Throw Errors from HTTP layer

--- src/api.js
+++ src/api.js
@@ -8,18 +8,26 @@ export const router = Router();
 router.post("/clientes/:id/transacoes", async (req, res) => {
   const body = await req.body;
   try {
-    await isBodyValid(body);
+    if (!["d", "c"].includes(body.tipo)) {
+      return res.status(422).json({ error: "Tipo Inválido" });
+    }
+    if (!Number.isInteger(body.valor) || body.valor <= 0) {
+      return res.status(422).json({ error: "Valor Inválido" });
+    }
+    if (!Boolean(body.descricao) || body.descricao.length > 10) {
+      return res.status(422).json({ error: "Descrição Inválida" });
+    }
 
     const id = req.params.id;
     const user = await getUser(id);
     if (user === null) {
-      throw new Error("Cliente não encontrado");
+      return res.status(404).json({ error: "Cliente não encontrado" });
     }
 
     const newBalance =
       user.saldo + (body.tipo === "d" ? -body.valor : body.valor);
     if (newBalance < -user.limite) {
-      throw new Error("Limite excedido");
+      return res.status(422).json({ error: "Limite excedido" });
     }
 
     await updateUser(
@@ -36,9 +44,6 @@ router.post("/clientes/:id/transacoes", async (req, res) => {
 
     return res.status(200).json({ limite: user.limite, saldo: newBalance });
   } catch (error) {
-    if (error.message === "Cliente não encontrado") {
-      return res.status(404).json({ error: error.message });
-    }
     return res.status(422).json({ error: error.message });
   } finally {
     console.log(`Transação: POST -> body: ${JSON.stringify(body)}\n`);
@@ -51,7 +56,7 @@ router.get("/clientes/:id/extrato", async (req, res) => {
   try {
     const user = await getUser(id);
     if (user === null) {
-      throw new Error("Cliente não encontrado");
+      return res.status(404).json({ error: "Cliente não encontrado" });
     }
     const balance = {
       total: user.saldo,
@@ -62,9 +67,6 @@ router.get("/clientes/:id/extrato", async (req, res) => {
       .status(200)
       .json({ saldo: balance, ultimas_transacoes: user.ultimas_transacoes });
   } catch (error) {
-    if (error.message === "Cliente não encontrado") {
-      return res.status(404).json({ error: error.message });
-    }
     return res.status(422).json({ error: error.message });
   } finally {
     console.log(`\nExtrato: GET -> id: ${id}\n`);
@@ -76,16 +78,16 @@ router.delete("/reset", async (_, res) => {
   res.status(200).json({ message: "Users reseted" });
 });
 
-async function isBodyValid(body) {
-  if (!["d", "c"].includes(body.tipo)) {
-    throw new Error("Tipo Inválido");
-  }
-  if (!Number.isInteger(body.valor) || body.valor <= 0) {
-    throw new Error("Valor Inválido");
-  }
-  if (!Boolean(body.descricao) || body.descricao.length > 10) {
-    throw new Error("Descrição Inválida");
-  }
-}
+// async function isBodyValid(body) {
+//   if (!["d", "c"].includes(body.tipo)) {
+//     throw new Error("Tipo Inválido");
+//   }
+//   if (!Number.isInteger(body.valor) || body.valor <= 0) {
+//     throw new Error("Valor Inválido");
+//   }
+//   if (!Boolean(body.descricao) || body.descricao.length > 10) {
+//     throw new Error("Descrição Inválida");
+//   }
+// }
 
 export default router;
